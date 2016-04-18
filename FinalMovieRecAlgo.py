73811872f90302d6f98e5c6b3409fbbdd553bb7c
unreal-kz <unreal-kz@users.noreply.github.com>
2016-04-18 19:07:04 -0400

    TOP 10 Movies Recommended

--- FinalMovieRecAlgo.py
+++ FinalMovieRecAlgo.py
@@ -1,6 +1,6 @@
 from pyspark import SparkConf, SparkContext
 
-def loadMovieNames():
+def loadNames():
     movieNames = {}
     with open("ml-100k/u.ITEM") as f:
         for line in f:
@@ -8,17 +8,17 @@ def loadMovieNames():
             movieNames[int(fields[0])] = fields[1]
     return movieNames
 
-conf = SparkConf().setMaster("local[*]").setAppName("PopularMovies")
+conf = SparkConf().setMaster("local[*]").setAppName("TopTenMovies")
 sc = SparkContext(conf = conf)
 
-nameDict = sc.broadcast(loadMovieNames())
+nameDict = sc.broadcast(loadNames())
 
 lines = sc.textFile("file:///ASP_RecomAlgoProject/ml-100k/u.data")
 movies = lines.map(lambda x: (int(x.split()[1]), 1))
 movieCounts = movies.reduceByKey(lambda x, y: x + y)
 
-flipped = movieCounts.map( lambda (x, y) : (y, x))
-sortedMovies = flipped.sortByKey(ascending = False)
+flip = movieCounts.map( lambda (x, y) : (y, x))
+sortedMovies = flip.sortByKey(ascending = False)
 
 sortedMoviesWithNames = sortedMovies.map(lambda (count, movie) : (nameDict.value[movie], count))
 
